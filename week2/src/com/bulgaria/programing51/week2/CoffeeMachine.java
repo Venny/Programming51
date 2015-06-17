1cd02f54cf1ececabc485b4e7b4144249a0c9d86
Venny <dim.veneta@gmail.com>
2015-06-17 19:44:02 +0300

    CoffeeMachine

--- week2/src/com/bulgaria/programing51/week2/CoffeeMachine.java
+++ week2/src/com/bulgaria/programing51/week2/CoffeeMachine.java
@@ -1,13 +1,11 @@
 package com.bulgaria.programing51.week2;
 
-import java.util.ArrayList;
-
 /**
  * Created by Inspired Day on 6/15/2015.
  */
 public class CoffeeMachine {
-    public int coffeeNum = 0;
-    public ArrayList<Coffee> coffeeList;
+    public static Coffee[] coffeeList = new Coffee[10];
+    public float balance = 0.0f;
 
     public static void main(String args[]){
         CoffeeMachine newMachine = new CoffeeMachine();
@@ -22,17 +20,29 @@ public class CoffeeMachine {
 
         newMachine.addCoffee(java);
         newMachine.addCoffee(moka);
-        System.out.println(newMachine.buyCoffee("Latte",3.40f));
+        System.out.println("Your change: " + newMachine.buyCoffee("Moka",3.40f));
+        System.out.println("Current balance: " + newMachine.balance);
     }
 
     public void addCoffee(Coffee coffee){
-        coffeeList.add(coffee);
+       // coffeeList.add(coffee);
+        for(int i = 0; i <= coffeeList.length; i++){
+           if(coffeeList[i] == null){
+               coffeeList[i] = coffee;
+               return;
+           }
+        }
     }
     public float buyCoffee(String name, float money){
-        if(coffeeList.contains(name)){
-
+        float change = 0;
+        for(int i = 0; i <= coffeeList.length; i++){
+            if( coffeeList[i] != null && coffeeList[i].brand.equals(name) ){
+                balance = money + balance;
+                change = money - coffeeList[i].price;
+                return change;
+            }
         }
-        return money;
+        return change;
     }
 
 }
